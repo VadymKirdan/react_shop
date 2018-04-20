a8b57ac2843214232afd2fd9d072ee97cecf95da
Vadym Kirdan <hogdubno@gmail.com>
2018-04-20 14:50:26 +0300

    jest - unit tests for redux products reducer

---
+++ src/tests/fixtures/products.js
@@ -0,0 +1,19 @@
+export default [{
+    id: '1',
+    type: 'mobile',
+    name: 'Nokia',
+    model: '3310',
+    price: 10000
+},{
+    id: '2',
+    type: 'mobile',
+    name: 'Lenovo',
+    model: 'T20',
+    price: 3000
+},{
+    id: '3',
+    type: 'mobile',
+    name: 'LG',
+    model: 'r2',
+    price: 7000
+}];
---
+++ src/tests/reducers/products.test.js
@@ -0,0 +1,66 @@
+import products from '../fixtures/products';
+import productsReducer from '../../reducers/products';
+
+test('should setup default state', () => {
+    const state = productsReducer(undefined, {type: '@@INIT'});
+    expect(state).toEqual([]);
+});
+
+test('should add product to store', () => {
+    const product = {
+        id: '4',
+        type: 'mobile',
+        name: 'Lenovo',
+        model: 'T210',
+        price: 4000
+    }
+    const action = {
+        type: 'ADD_PRODUCT',
+        product
+    }
+    const state = productsReducer(products, action);
+    expect(state).toEqual([...products, action.product]);
+});
+
+test('should remove product from store', () => {
+    const action = {
+        type: 'REMOVE_PRODUCT',
+        id: products[1].id
+    }
+    const state = productsReducer(products, action);
+    expect(state).toEqual([products[0], products[2]]);
+});
+
+test('should not remove products if id doesn\'t exist', () => {
+    const action = {
+        type: 'REMOVE_PRODUCT',
+        id: '5'
+    }
+    const state = productsReducer(products, action);
+    expect(state).toEqual(products);
+});
+
+test('should edit existing product in store', () => {
+    const action = {
+        type: 'EDIT_PRODUCT',
+        id: '2',
+        updateObj: {
+            type: 'smartphone'
+        }
+    }
+    const state = productsReducer(products, action);
+    expect(state[1].type).toBe('smartphone');
+});
+
+
+test('should not edit product in store if it doesn\'t exist', () => {
+    const action = {
+        type: 'EDIT_PRODUCT',
+        id: '6',
+        updateObj: {
+            type: 'smartphone'
+        }
+    }
+    const state = productsReducer(products, action);
+    expect(state).toEqual(products);
+});
