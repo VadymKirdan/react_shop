477898c1e6e5d559bbdeb79b2d38198b5134d7b3
Vadym Kirdan <hogdubno@gmail.com>
2018-06-25 15:51:36 +0300

    Product Form validation

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import FormErrors from '../components/FormErrors';
 
 export default class ProductForm extends React.Component {
     constructor(props) {
@@ -7,7 +8,13 @@ export default class ProductForm extends React.Component {
             type:  this.props.product ? this.props.product.type : '',
             name:  this.props.product ? this.props.product.name : '',
             model: this.props.product ? this.props.product.model : '',
-            price: this.props.product ? this.props.product.price : 0
+            price: this.props.product ? this.props.product.price : '',
+            formErrors: {
+                type: '',
+                name: '',
+                model: '',
+                price: ''
+            }
         }
         this.onTypeChange  = this.onTypeChange.bind(this);
         this.onNameChange  = this.onNameChange.bind(this);
@@ -33,22 +40,81 @@ export default class ProductForm extends React.Component {
     }
     onFormSubmit(e) {
         e.preventDefault();
-        this.props.onSubmit({
-            type:  this.state.type,
-            name:  this.state.name,
-            model: this.state.model,
-            price: parseFloat(this.state.price, 10)
-        });
+        const { type, name, model, price } = this.state;
+        const isValidForm = this.validateForm({type, name, model, price});
+      
+        if (isValidForm) {
+            this.props.onSubmit({
+                type,
+                name,
+                model,
+                price: parseFloat(price, 10)
+            });
+        }
     }
+    validateForm(product) {
+        let isValidType;
+        let isValidName;
+        let isValidModel;
+        let isValidPrice;
+        const validationErrors = {};
+
+        Object.keys(product).map((field) => {
+            const fieldValue = product[field];
+            switch(field) {
+                case 'type':
+                    isValidType = fieldValue.length >= 4;
+                    validationErrors.type = isValidType ? '' : 'type is too short';
+                    break;
+                case 'name':
+                    isValidName = fieldValue.length >= 2;
+                    validationErrors.name = isValidName ? '' : 'name is too short';
+                    break;
+                case 'model':
+                    isValidModel = fieldValue.length >= 2;
+                    validationErrors.model = isValidModel ? '' : 'model is too short';
+                    break;
+                case 'price':
+                    var numberRegex = /^[0-9]+$/;
+                    isValidPrice = fieldValue.match(numberRegex);
+                    validationErrors.price = isValidPrice ? '' : 'price is not valid';
+                    break;
+                default:
+                    break;
+            }
+        });
+
+        this.setState(() => 
+            (
+                { formErrors: {
+                        type: validationErrors.type,
+                        name: validationErrors.name,
+                        model: validationErrors.model,
+                        price: validationErrors.price 
+                    }
+                }
+            )
+        );
+
+        if (!isValidType || !isValidName || !isValidModel || !isValidPrice) {
+            return false;
+        }
+
+        return true;
+    }
+    
     render() {
         return (
             <div>
-                <form onSubmit={this.onFormSubmit}>
-                    Type:  <input type="text" onChange={this.onTypeChange}  value={this.state.type}  /><br />
-                    Name:  <input type="text" onChange={this.onNameChange}  value={this.state.name}  /><br />
-                    Model: <input type="text" onChange={this.onModelChange} value={this.state.model} /><br />
-                    Price: <input type="text" onChange={this.onPriceChange} value={this.state.price} /><br />
-                    <button>{this.props.product ? "Update Product" : "Add Product"}</button>
+                <form className="product-form" onSubmit={this.onFormSubmit}>
+                    <FormErrors formErrors={this.state.formErrors} />
+                    <input type="text" placeholder="Type" className="form-input" onChange={this.onTypeChange}  value={this.state.type}  />
+                    <input type="text" placeholder="Name" className="form-input" onChange={this.onNameChange}  value={this.state.name}  />
+                    <input type="text" placeholder="Model" className="form-input" onChange={this.onModelChange} value={this.state.model} />
+                    <input type="text" placeholder="Price" className="form-input" onChange={this.onPriceChange} value={this.state.price} />
+                    <div>
+                        <button className="form-button">{this.props.product ? "Update Product" : "Save Product"}</button>
+                    </div>
                 </form>
             </div>
         )
